f84f829ad3bea7c3c08d5d14ab953e861b3e5b84
jlahire <lahire.jack@gmail.com>
2025-11-14 10:52:39 -0500

    Staging for build

---
+++ dfd.py
@@ -0,0 +1,30 @@
+# Deep fake detector for free tier reality defender API
+
+class DeepfakeDetector:
+    
+
+    def __init__():
+    
+    def checkFile():
+
+    def analyze():
+
+    def results():
+
+    def score():
+
+    
+class Applicaiton:
+
+    def __init__():
+
+    def getAPI():
+
+    def getFile():
+
+    def run():
+
+def main():
+
+if __name__ == "__main__":
+    main()
